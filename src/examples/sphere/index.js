5f314728094c769f6a6e5697f027ff7337e3e6ff
srm <srm@laufire.in>
2022-02-06 12:19:59 +0530

    Made shadow configurable.

--- src/examples/sphere/index.js
+++ src/examples/sphere/index.js
@@ -1,23 +1,22 @@
 /* eslint-disable no-magic-numbers */
 import React from 'react';
 import { useControls } from 'leva';
-import { Plane, Sphere } from '@react-three/drei';
+import { Plane } from '@react-three/drei';
 
 // eslint-disable-next-line max-lines-per-function
 const control = () => {
 	const dirLightProps = useControls('Directional Light', {
+		angle: { value: 0, min: 0, max: 180, step: 15 },
+		distance: { value: 7, min: 7, max: 10, step: 1 },
 		castShadow: true,
 		receiveShadow: false,
 		intensity: { value: 1, min: 0.5, max: 5, step: 0.5 },
-		position: [-10, 8, -5],
 	});
 
 	const sphereProps = useControls('Sphere', {
-		castShadow: true,
-		receiveShadow: false,
 		rotation: [0, 0, 0],
-		position: [0, 0, 0.5],
-		args: [0.8, 60, 60],
+		position: [0, 0, 0],
+		args: [0.8, 15, 15],
 		color: '#ff6000',
 		scale: { value: 1, min: 0.5, max: 5, step: 0.5 },
 	});
@@ -27,28 +26,40 @@ const control = () => {
 		castShadow: false,
 		rotation: [-1.4, 0, 0],
 		position: [0, -1, 0],
-		args: [-8, -5, 1],
+		args: [500, 500],
 		color: '#f0b18b',
 	});
 
 	return { dirLightProps, sphereProps, planeProps };
 };
 
+const degToPos = (angle, distance) =>
+	({ x: distance * Math.cos(angle), y: distance * Math.sin(angle) });
+
+const changeAngle = (angle) => 180 - angle;
+
+// eslint-disable-next-line max-lines-per-function
 const DemoSphere = () => {
 	const { planeProps, dirLightProps, sphereProps } = control();
 	const { color, ...sProps } = sphereProps;
 	const { color: pColor, ...pProps } = planeProps;
+	const { angle, distance } = dirLightProps;
+	const { x, y } = degToPos(changeAngle(angle), distance);
 
-	return <mesh>
+	return <>
 		<ambientLight/>
-		<directionalLight { ...dirLightProps }/>
-		<Sphere { ...sProps }>
+		<directionalLight position={ [x, y, 0] } { ...dirLightProps }/>
+		<mesh
+			castShadow={ true }
+			receiveShadow={ false }
+		>
+			<sphereBufferGeometry { ...sProps }/>
 			<meshStandardMaterial color={ color }/>
-		</Sphere>
+		</mesh>
 		<Plane { ...pProps }>
 			<meshStandardMaterial color={ pColor }/>
 		</Plane>
-	</mesh>;
+	</>;
 };
 
 export default DemoSphere;
