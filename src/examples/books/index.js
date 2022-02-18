6f6320c99ce5825b987a3a3abb0727cd5c6e9259
srm <srm@laufire.in>
2022-02-18 15:21:06 +0530

    Introduced texture for RotatingBooks.

--- src/examples/books/index.js
+++ src/examples/books/index.js
@@ -3,18 +3,22 @@ import { useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useControls } from 'leva';
 import { React, useRef } from 'react';
-import image from '../../images/bookCover.jpg';
 
 const control = () => useControls('Book', {
 	scale: 0.25,
 	args: [7, 10, 1.5],
+	color: '0xffffff',
 	positionOne: [0.75, 0.5, 0],
 	positionTwo: [-0.75, 0.5, 0],
 });
 
 const Book = ({ position }) => {
-	const { scale, args } = control();
-	const [colorMap] = useTexture([image]);
+	const { scale, args, color } = control();
+	const bookCover = useTexture(`${ process.env.PUBLIC_URL }/bookCover.jpg`);
+	const bookBack = useTexture(`${ process.env.PUBLIC_URL }/bookBack.jpg`);
+	const bookSide = useTexture(`${ process.env.PUBLIC_URL }/bookSide.jpg`);
+	const bookSpine = useTexture(`${ process.env.PUBLIC_URL }/bookSpine.jpg`);
+
 	const mesh = useRef();
 
 	useFrame(() => {
@@ -24,7 +28,12 @@ const Book = ({ position }) => {
 
 	return <mesh ref={ mesh } scale={ scale } position={ position }>
 		<boxBufferGeometry args={ args }/>
-		<meshLambertMaterial map={ colorMap }/>
+		<meshLambertMaterial color={ color } map={ bookCover }/>
+		<meshLambertMaterial map={ bookBack }/>
+		<meshLambertMaterial map={ bookSide }/>
+		<meshLambertMaterial map={ bookSide }/>
+		<meshLambertMaterial map={ bookCover }/>
+		<meshLambertMaterial map={ bookSpine }/>
 	</mesh>;
 };
 
