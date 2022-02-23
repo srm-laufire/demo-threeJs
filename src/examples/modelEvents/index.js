7eaf1a5415822c07b450418cdc6adb38a8df5099
srm <srm@laufire.in>
2022-02-23 14:52:52 +0530

    Worked onClick for the model, flight.

--- src/examples/modelEvents/index.js
+++ src/examples/modelEvents/index.js
@@ -9,40 +9,21 @@ import { useControls } from 'leva';
 const control = () => {
 	const flightProps = useControls('Flight', {
 		flight: true,
-		x: { value: -2, min: -2, max: 2, step: 0.2 },
+		x: { value: 0, min: -2, max: 2, step: 0.2 },
 		y: { value: 0, min: -2, max: 2, step: 0.2 },
 		z: { value: 1.5, min: 1.5, max: 5, step: 0.2 },
 		rotation: [0, 0, 0],
 	});
 
-	const humanProps = useControls('Human', {
-		human: true,
-		x: { value: 2, min: -2, max: 2, step: 0.2 },
-		y: { value: -1, min: -2, max: 2, step: 0.2 },
-		z: { value: 1.5, min: 1.5, max: 5, step: 0.2 },
-		rotation: [0, 0, 0],
-	});
-
-	return { flightProps, humanProps };
+	return { flightProps };
 };
 
 const ModelEvents = (context) => {
 	const { state: { clicked }, patchState } = context;
 	const { flightProps: { x: xFlight, y: yFlight,
-		z: zFlight, rotation, flight },
-	humanProps: { x: xHuman, y: yHuman, z: zHuman,
-		rotation: hRotation, human }} = control();
+		z: zFlight, rotation, flight }} = control();
 	const flightGltf = useLoader(GLTFLoader,
 		`${ process.env.PUBLIC_URL }/flight.gltf`);
-	const humanGltf = useLoader(GLTFLoader,
-		`${ process.env.PUBLIC_URL }/human.gltf`);
-	const gltfHumanProps = {
-		onClick: () => patchState({ clicked: !clicked }),
-		object: humanGltf.scene,
-		scale: 1.5,
-		position: [xHuman, yHuman, zHuman],
-		rotation: clicked ? [2, 0, 0] : hRotation,
-	};
 	const gltfFlightProps = {
 		onClick: () => patchState({ clicked: !clicked }),
 		object: flightGltf.scene,
@@ -55,7 +36,6 @@ const ModelEvents = (context) => {
 		<>
 			<ambientLight/>
 			{ flight && <primitive { ...gltfFlightProps }/> }
-			{ human && <primitive { ...gltfHumanProps }/> }
 		</>
 	);
 };
